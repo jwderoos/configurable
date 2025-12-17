00c12075066217c1d760b7991fa6f0c0b31f5c29
JW de Roos <janwillem@jwdr.nl>
2025-12-17 23:04:21 +0100

    3 - adds autoloading of configurable services

--- src/DependencyInjection/ConfigurableExtension.php
+++ src/DependencyInjection/ConfigurableExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace jwderoos\Configurable\DependencyInjection;
 
+use jwderoos\Configurable\Interface\ConfigurableServiceInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -13,6 +14,9 @@ class ConfigurableExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
+        $container->registerForAutoconfiguration(ConfigurableServiceInterface::class)
+            ->addTag('jwderoos.configurable.service');
+
         $yamlFileLoader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__ . '/../Resources/config')
